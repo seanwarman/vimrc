93d9fb3cc2043ac042d35f182d392e6450ca3a59
Sean Warman <seanwarman@gmail.com>
2021-09-27 14:24:26 +0100

    Added install list

---
+++ install.sh
@@ -0,0 +1,11 @@
+vim
+nvm
+brew
+fzf
+bat
+oh-my-zsh
+ssh for gitlab and github
+iterm2 (with Profiles)
+docker
+Brave
+clone weasel, weasel-adapter, nomcom, ucas-frontend-server, charcoal-blue, events-cms
